d7d9f6f9941969a9d30c3483fca508f9821e25f8
janemirn <black.hot.chili.panda@gmail.com>
2020-11-13 12:56:04 +0300

    lab 2

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <?php
 
-class A {
+class A
+{
+    protected $X;
+
+    public function equation($a, $b)
+    {
+        return $a == 0 ? "a = 0!" : $this->X = -$b / $a;
+    }
 }
 
-class B extends A {
-	public function __construct ($a, $b) {
-	   $this -> a = $a;
-	   $this -> b = $b;
-	}
-	 protected $a;
-	 protected $b;
-}
+class B extends A
+{
+    protected $D;
 
-class C extends B {
-	public function __construct($a, $b, $c) {
-	   $this -> c = $c;
-	   parent::__construct($a, $b);
-   }
-   protected $c;
-}
+    protected function discriminant($a, $b, $c)
+    {
+        return $this->D = ($b *$b) -4 * $a * $c;
+    }
+
+    public function qu_equation($a, $b, $c)
+    {
+        if ($a == 0) {
+            return $this->equation($b, $c);
+        }
+
+        $D = $this->discriminant($a, $b, $c);
+
+        if ($D > 0) {
+            $X1 = (-$b + sqrt($D)) / (2 * $a);
+            $X2 = (-$b - sqrt($D) )/ (2 * $a);
+            return array($X1, $X2);
+        }
+        if ($D == 0) {
+            return $X1 = (-$b / 2 * $a);
+        }
